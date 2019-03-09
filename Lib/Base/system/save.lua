be901b8b0590689951aebb890de9ca9d0da62a69
Pete Hunter <pete.hunter@piksel.com>
2019-03-09 13:48:58 +0000

    Updated save functions

--- Lib/Base/system/save.lua
+++ Lib/Base/system/save.lua
@@ -1,6 +1,6 @@
 return function(data, path, name)
   love.filesystem.createDirectory(path)
-  local save = love.filesystem.newFile(path.."/"..name..".lua", "w")
+  local save = io.open(path.."/"..name..".lua", "w")
   save:write("return "..engine.string.serialize(data))
   save:close()
   engine.log("Data saved to "..path.."/"..name..".lua")
