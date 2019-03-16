ab6d2a2593b15d5470bd964af6b6463e0904e7ba
Pete Hunter <pete.hunter@piksel.com>
2019-03-16 16:08:42 +0000

    Behaviour update.

--- Data/Solar System/Planet/component/planet.lua
+++ Data/Solar System/Planet/component/planet.lua
@@ -5,9 +5,9 @@ function pl.new(size, texture)
     ground = texture or "rock",
     clouds = "white",
     cloud_angle = 0,
-    ground_id = math.ceil(love.math.random(1, engine.texture_variation)),
-    clouds_id = math.ceil(love.math.random(1, engine.texture_variation)),
-    shade_id = math.ceil(love.math.random(1, engine.texture_variation)),
+    ground_id = math.ceil(math.random(1, engine.texture_variation)),
+    clouds_id = math.ceil(math.random(1, engine.texture_variation)),
+    shade_id = math.ceil(math.random(1, engine.texture_variation)),
     size = size,
   }
 end
