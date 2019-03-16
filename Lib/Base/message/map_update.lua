50ca43d8a6eecff52295cd405b8f0f2b26cc7439
Pete Hunter <pete.hunter@piksel.com>
2019-03-16 23:06:05 +0000

    Added entity grid to update packet.

--- Lib/Base/message/map_update.lua
+++ Lib/Base/message/map_update.lua
@@ -2,7 +2,6 @@ return function(event, packet)
   local pkt = {
     command = "map_update",
     data = game.ecs.entity_list,
-    e_list = game.entity_grid,
   }
   event.peer:send(engine.string.serialize(pkt))
 end
