5e471dba56a2c4865031a12b4f23bb3b5c5ca32d
Pete Hunter <pete.hunter@piksel.com>
2019-03-22 12:28:58 +0000

    Broadcast test

--- Lib/ECS/System_Features/Collisions/behaviour/collide.lua
+++ Lib/ECS/System_Features/Collisions/behaviour/collide.lua
@@ -2,24 +2,25 @@ local collide = engine.class:extend()
       collide.requirements = {"body", "collider"}
 
 function collide:init()
-  game.entity_grid = engine.system.spatial_hash:new(engine.hash_grid_size)
+  game.entity_grid = {engine.system.spatial_hash:new(engine.hash_grid_size)}
 end
 
 function collide:pre_update(dt)
-  game.entity_grid = engine.system.spatial_hash:new(engine.hash_grid_size)
+  table.insert(game.entity_grid, engine.system.spatial_hash:new(engine.hash_grid_size))
+  table.remove(game.entity_grid, 1)
 end
 
 function collide:update(dt, ent)
-  game.entity_grid:add(ent.body.x, ent.body.y, ent.body.w, ent.body.h, ent.id)
+  game.entity_grid[1]:add(ent.body.x, ent.body.y, ent.body.w, ent.body.h, ent.id)
 end
 
 function collide:post_update(dt)
   local collision_checks = 0
-  local list = game.entity_grid.warnings
+  local list = game.entity_grid[1].warnings
   if list[1] == nil then return end
   for i = 1, #list do
     local ch_pos = list[i]
-    local e_list = game.entity_grid.cells[ch_pos.x][ch_pos.y]
+    local e_list = game.entity_grid[1].cells[ch_pos.x][ch_pos.y]
     if #e_list > 1 then
       for i = 1, #e_list do
         local e_1 = game.ecs:get_entity(e_list[i])
